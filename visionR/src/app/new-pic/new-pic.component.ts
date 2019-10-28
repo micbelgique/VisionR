3581338b0463578716f643f101e5aac0af575dd1
Nathan <nathan.pire@gmail.com>
2019-10-28 14:14:32 +0100

    creating the project and adding angular material

---
+++ visionR/src/app/new-pic/new-pic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-pic',
+  templateUrl: './new-pic.component.html',
+  styleUrls: ['./new-pic.component.css']
+})
+export class NewPicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
